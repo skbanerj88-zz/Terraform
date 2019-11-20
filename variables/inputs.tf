a8271256da58a2f215065ace2b6c3c17fc590361
Sourish Kumar Banerjee <13816308+skbanerj88@users.noreply.github.com>
2019-11-20 10:21:15 +0000

    Update inputs.tf

--- variables/inputs.tf
+++ variables/inputs.tf
@@ -5,3 +5,28 @@ variable "environment" {
 variable "size" {
   description = "The size of the instances"
   }
+
+variable “subnet_map” {
+  description = “A map from environment to a comma-delimited list of the subnets”
+  type = “map”
+default = {
+    dev     = “subnet-c59403abe,subnet-69483bdb33c”
+    qa      = “subnet-e48unjd9a1,subnet-c085uhd93a4”
+  }
+}
+output “subnets” {
+  value = [“${split(“,”, var.subnet_map[var.environment])}”]
+} 
+ 
+variable “instance_type_map” {
+  description = “A map from environment to the type of EC2 instance”
+  type = “map”
+  default = {
+    small  = “t2.large”,
+    medium = “t2.xlarge”
+  }
+}
+output “instance_type” {
+  value = “${var.instance_type_map[var.size]}”
+} 
+ 
